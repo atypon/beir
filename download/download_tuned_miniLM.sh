99c45c87b0b3d677c08417d905d0d2773e6038bd
stevaras2 <sdstefanid@wiley.com>
2022-07-01 12:13:52 +0300

    conduct experiemnts for the tuned miniLM model

--- download/download_tuned_miniLM.sh
+++ download/download_tuned_miniLM.sh
@@ -5,4 +5,4 @@ gsutil cp gs://ai_repo/specter/semantic_scholar_dataset_2hops/specter_like/all_m
 gsutil cp gs://ai_repo/specter/semantic_scholar_dataset_2hops/specter_like/all_minilm_l12_v2_multi_objective/tokenizer_config.json model/tuned_miniLM/
 gsutil cp gs://ai_repo/specter/semantic_scholar_dataset_2hops/specter_like/all_minilm_l12_v2_multi_objective/tokenizer.json model/tuned_miniLM/
 gsutil cp gs://ai_repo/specter/semantic_scholar_dataset_2hops/specter_like/all_minilm_l12_v2_multi_objective/vocab.txt model/tuned_miniLM/
-gsutil cp gs://ai_repo/specter/semantic_scholar_dataset_2hops/specter_like/all_minilm_l12_v2_multi_objective/special_tokens_map.json.json model/tuned_miniLM/
+gsutil cp gs://ai_repo/specter/semantic_scholar_dataset_2hops/specter_like/all_minilm_l12_v2_multi_objective/special_tokens_map.json model/tuned_miniLM/
