bebce06161239641b554117269d989e65b8c9d5f
stevaras2 <sstefanidis@atypon.com>
2023-06-07 11:47:34 +0300

    fix miniLM downloader

--- download/download_miniLM.sh
+++ download/download_miniLM.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir model
 mkdir model/miniLM
 gsutil cp -r gs://ai_repo/specter/knowledge-distillation/all-MiniLM-L12-v2/tokenizer/tokenizer/* model/miniLM/
-gsutil cp gs://ai_repo/specter/knowledge-distillation/all-MiniLM-L12-v2/all_minilm_l12_fp16_optimized.onnx model/miniLM
+gsutil cp gs://ai_repo/specter/knowledge-distillation/all-MiniLM-L12-v2/all_minilm_l12_fp16_optimized.onnx model/miniLM/
