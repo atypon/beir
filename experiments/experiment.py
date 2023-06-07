ba72da738d7d75d82367a3654fdb20dd5a05a337
stevaras2 <sstefanidis@atypon.com>
2023-06-07 14:16:26 +0300

    Add an extra step to rerank results with cross encoder

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -5,6 +5,8 @@ from mlflow.tracking import MlflowClient
 from beir.retrieval.models import OnnxBERT
 from beir.retrieval.search.lexical import BM25Search as BM25
 from beir.datasets.data_loader import GenericDataLoader
+from beir.reranking.models.cross_encoder import CrossEncoder
+from beir.reranking import Rerank
 from beir.retrieval.evaluation import EvaluateRetrieval
 from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
 
@@ -47,23 +49,7 @@ class Experiment(object):
             try:
                 corpus, queries, qrels = GenericDataLoader(data_folder=dataset).load(split='test')
                 results = self.retriever.retrieve(corpus=corpus, queries=queries)
-                ndcg, _map, recall, precision = self.retriever.evaluate(qrels=qrels,
-                                                                        results=results,
-                                                                        k_values=self.retriever.k_values)
-                ndcg = self._rename_metrics(metric_score=ndcg)
-                _map = self._rename_metrics(metric_score=_map)
-                recall = self._rename_metrics(metric_score=recall)
-                precision = self._rename_metrics(metric_score=precision)
-                flatten_metrics = self._concat_metrics(ndcg=ndcg,
-                                                       recall=recall,
-                                                       _map=_map,
-                                                       precision=precision)
-                self._track_metric(dataset=dataset, metric_score=flatten_metrics)
-                print('Results for', dataset)
-                print('NDCG:', ndcg)
-                print("Recall:", recall)
-                print('Precision:', precision)
-                print('MAP:', _map)
+                self._eval_pipeline(qrels=qrels, results=results, dataset=dataset)
             except:
                 print('There is an error in this dataset:', dataset)
 
@@ -89,6 +75,34 @@ class Experiment(object):
             flatten_metrics.update(metric)
         return flatten_metrics
 
+    def _eval_pipeline(self,
+                       qrels: Dict[str, Dict[str, int]],
+                       results: Dict[str, Dict[str, float]],
+                       dataset: str) -> None:
+        """
+        Evaluation of the results of a pipeline and log them in MLFlow
+        :param qrels: the relevance of each query-doc pair
+        :param results: the results of the pipeline
+        :param dataset: the dataset name
+        """
+        ndcg, _map, recall, precision = self.retriever.evaluate(qrels=qrels,
+                                                                results=results,
+                                                                k_values=self.retriever.k_values)
+        ndcg = self._rename_metrics(metric_score=ndcg)
+        _map = self._rename_metrics(metric_score=_map)
+        recall = self._rename_metrics(metric_score=recall)
+        precision = self._rename_metrics(metric_score=precision)
+        flatten_metrics = self._concat_metrics(ndcg=ndcg,
+                                               recall=recall,
+                                               _map=_map,
+                                               precision=precision)
+        self._track_metric(dataset=dataset, metric_score=flatten_metrics)
+        print('Results for', dataset)
+        print('NDCG:', ndcg)
+        print("Recall:", recall)
+        print('Precision:', precision)
+        print('MAP:', _map)
+
 
 class RerankExperiment(Experiment):
     def __init__(self,
@@ -106,7 +120,7 @@ class RerankExperiment(Experiment):
         self.initialize = initialize
         super().__init__(datasets, datasets_path, onnx_model, batch_size, score_function, mlflow_configs)
 
-    def __create_bm25_retriever(self, index_name):
+    def _create_bm25_retriever(self, index_name) -> EvaluateRetrieval:
         model = BM25(index_name=index_name, hostname=self.es_hostname, initialize=self.initialize)
         retriever = EvaluateRetrieval(model)
         return retriever
@@ -116,28 +130,97 @@ class RerankExperiment(Experiment):
             try:
                 corpus, queries, qrels = GenericDataLoader(data_folder=dataset).load(split='test')
                 index_name = dataset.replace('/', '_')
-                bm25_retriever = self.__create_bm25_retriever(index_name=index_name)
+                bm25_retriever = self._create_bm25_retriever(index_name=index_name)
                 bm25_results = bm25_retriever.retrieve(corpus=corpus, queries=queries)
                 rerank_results = self.retriever.rerank(corpus=corpus,
                                                        queries=queries,
                                                        results=bm25_results,
                                                        top_k=self.k)
-                ndcg, _map, recall, precision = self.retriever.evaluate(qrels=qrels,
-                                                                        results=rerank_results,
-                                                                        k_values=self.retriever.k_values)
-                ndcg = self._rename_metrics(metric_score=ndcg)
-                _map = self._rename_metrics(metric_score=_map)
-                recall = self._rename_metrics(metric_score=recall)
-                precision = self._rename_metrics(metric_score=precision)
-                flatten_metrics = self._concat_metrics(ndcg=ndcg,
-                                                       recall=recall,
-                                                       _map=_map,
-                                                       precision=precision)
-                self._track_metric(dataset=dataset, metric_score=flatten_metrics)
-                print('Results for', dataset)
-                print('NDCG:', ndcg)
-                print("Recall:", recall)
-                print('Precision:', precision)
-                print('MAP:', _map)
+                self._eval_pipeline(qrels=qrels, results=rerank_results, dataset=dataset)
+            except:
+                print('There is an error in this dataset:', dataset)
+
+
+class RerankBiCrossEncodersExperiment(RerankExperiment):
+    """
+    An extention class where the results of the RerankExperiment pipeline will be reranked based on a cross encoder
+    """
+    def __init__(self,
+                 datasets: List[str],
+                 datasets_path: str,
+                 onnx_model: OnnxBERT,
+                 ce_model: str,
+                 bi_batch_size: int,
+                 ce_batch_size: int,
+                 top_k: int,
+                 score_function: str,
+                 es_hostname: str,
+                 initialize: bool,
+                 mlflow_configs: Dict[str, str]):
+        """
+        Initialize the class by load ing the models
+        :param datasets: a list with the datasets to evaluate
+        :param datasets_path: the path we stored the datasets
+        :param onnx_model: the onnx bi-encoder
+        :param ce_model: the hf card of the cross encoder model
+        :param bi_batch_size: the batch size for the bi-encoder step.
+        :param ce_batch_size: the batch size for the cross-encoder step
+        :param top_k: retrieve top_k results using the bi-encoder
+        :param score_function: the similarity metric
+        :param es_hostname: the hostname of ElasticSearch
+        :param initialize: a boolean to decide if we will initialize the ES or not
+        :param mlflow_configs: the MLFlow server configurations
+        """
+        self.ce = CrossEncoder(ce_model)
+        self.ce_batch_size = ce_batch_size
+        self.reranker = Rerank(model=self.ce, batch_size=self.ce_batch_size)
+        super().__init__(datasets=datasets,
+                         datasets_path=datasets_path,
+                         onnx_model=onnx_model,
+                         top_k=top_k,
+                         batch_size=bi_batch_size,
+                         score_function=score_function,
+                         es_hostname=es_hostname,
+                         initialize=initialize,
+                         mlflow_configs=mlflow_configs)
+
+    def __rerank_pipeline(self,
+                          corpus: Dict[str, Dict[str, str]],
+                          queries: Dict[str, str],
+                          index_name: str) \
+            -> Dict[str, Dict[str, float]]:
+        """
+        perform all the rerank steps of the pipeline
+        :param corpus: the corpus of a specific dataset
+        :param queries: the queries of this dataset
+        :param index_name: the name of the ES index
+        :return  the reranked results.
+        """
+        bm25_retriever = self._create_bm25_retriever(index_name=index_name)
+        bm25_results = bm25_retriever.retrieve(corpus=corpus, queries=queries)
+        bi_rerank_results = self.retriever.rerank(corpus=corpus,
+                                                  queries=queries,
+                                                  results=bm25_results,
+                                                  top_k=self.k)
+        ce_rerank_results = self.reranker.rerank(corpus=corpus,
+                                                 queries=queries,
+                                                 results=bi_rerank_results,
+                                                 top_k=self.k)
+        return ce_rerank_results
+
+    def experiment_pipeline(self):
+        """
+        The full pipeline of the experiment. The steps of this pipeline are:
+        1) Retrieve documents using BM25
+        2) Rerank them using the embeddings extracted by a bi-encoder
+        3) Rerank the previous results using a cross encoder
+        Finally, evaluate the results and log the metrics to MLFlow server.
+        """
+        for dataset in self.dataset_paths:
+            try:
+                corpus, queries, qrels = GenericDataLoader(data_folder=dataset).load(split='test')
+                index_name = dataset.replace('/', '_')
+                rerank_results = self.__rerank_pipeline(corpus=corpus, queries=queries, index_name=index_name)
+                self._eval_pipeline(qrels=qrels, results=rerank_results, dataset=dataset)
             except:
                 print('There is an error in this dataset:', dataset)
