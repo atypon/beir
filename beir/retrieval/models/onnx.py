e9408384256743ebd6ec5ca75a4b021b926d6033
stevaras2 <sstefanidis@atypon.com>
2023-09-22 16:40:18 +0300

    Implement the BGE model

--- beir/retrieval/models/onnx.py
+++ beir/retrieval/models/onnx.py
@@ -7,11 +7,11 @@ import numpy as np
 
 
 class OnnxBERT:
-    def __init__(self,
-                 onnx_filename: Union[str, Tuple],
-                 model_path: Union[str, Tuple] = None,
-                 sep: str = " ",
-                 **kwargs):
+    def _init_(self,
+               onnx_filename: Union[str, Tuple],
+               model_path: Union[str, Tuple] = None,
+               sep: str = " ",
+               **kwargs):
         self.sep = sep
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
         session_options = SessionOptions()
@@ -34,14 +34,14 @@ class OnnxBERT:
                        convert_to_tensor: bool = None,
                        show_progress_bar: bool = True) -> Union[
         List[List[float]], np.ndarray, List[float]]:
-        batchified_queries = self.__batchify(queries=queries, batch_size=batch_size)
+        batchified_queries = self._batchify(queries=queries, batch_size=batch_size)
         query_embeddings = []
         if show_progress_bar:
             batches = tqdm(batchified_queries, total=len(batchified_queries))
         else:
             batches = tqdm(batchified_queries)
         for batch in batches:
-            inputs = self.__create_ort_input(queries=batch)
+            inputs = self._create_ort_input(queries=batch)
             query_embeddings += list(self.q_model.run([], inputs)[0])
         query_embeddings = np.asarray(query_embeddings)
         return query_embeddings
@@ -54,19 +54,19 @@ class OnnxBERT:
         List[List[float]], np.ndarray, List[float]]:
         sentences = [(doc["title"] + self.sep + doc["text"]).strip() if "title" in doc else doc["text"].strip() for doc
                      in corpus]
-        batchified_sentences = self.__batchify(queries=sentences, batch_size=batch_size)
+        batchified_sentences = self._batchify(queries=sentences, batch_size=batch_size)
         corpus_embeddings = []
         if show_progress_bar:
             batches = tqdm(batchified_sentences, total=len(batchified_sentences))
         else:
             batches = tqdm(batchified_sentences)
         for batch in batches:
-            inputs = self.__create_ort_input(queries=batch)
+            inputs = self._create_ort_input(queries=batch)
             corpus_embeddings += list(self.doc_model.run([], inputs)[0])
         corpus_embeddings = np.asarray(corpus_embeddings)
         return corpus_embeddings
 
-    def __tokenize_text(self, queries: List[str]) -> Tuple[np.ndarray, np.ndarray]:
+    def _tokenize_text(self, queries: List[str]) -> Tuple[np.ndarray, np.ndarray]:
         encoding = self.tokenizer(queries,
                                   return_token_type_ids=False,
                                   return_tensors='np',
@@ -77,16 +77,57 @@ class OnnxBERT:
         attention_mask = encoding['attention_mask']
         return input_ids, attention_mask
 
-    def __create_ort_input(self, queries: List[str]) -> Dict[str, List[np.ndarray]]:
-        ort_input_ids, ort_attention_mask = self.__tokenize_text(queries=queries)
+    def _create_ort_input(self, queries: List[str]) -> Dict[str, List[np.ndarray]]:
+        ort_input_ids, ort_attention_mask = self._tokenize_text(queries=queries)
         inputs = {
             'input_ids': list(ort_input_ids),
             'attention_mask': list(ort_attention_mask)
         }
         return inputs
 
-    def __batchify(self, queries: List[str], batch_size: int) -> List[List[str]]:
+    @staticmethod
+    def _batchify(queries: List[str], batch_size: int) -> List[List[str]]:
         batches = []
         for i in range(0, len(queries), batch_size):
             batches.append(queries[i: i+batch_size])
         return batches
+
+
+class OnnxBGE(OnnxBERT):
+    """
+    Load BGE model into Inference sessions. The difference with the OnnxBERT class is that in the query BGE prepends
+    an instruction at the beginning to let the model extract better embeddings for queries.
+    """
+    def __init__(self,
+                 onnx_filename: Union[str, Tuple],
+                 query_instruction: str,
+                 model_path: Union[str, Tuple] = None,
+                 sep: str = " ",
+                 enable_query_instruction: bool = False,
+                 **kwargs):
+        self.query_instruction = query_instruction
+        self.enable_query_instruction = enable_query_instruction
+        super().__init__(onnx_filename=onnx_filename,
+                         model_path=model_path,
+                         sep=sep,
+                         kwargs=kwargs)
+
+    def encode_queries(self,
+                       queries: List[str],
+                       batch_size: int,
+                       convert_to_tensor: bool = None,
+                       show_progress_bar: bool = True) -> Union[List[List[float]], np.ndarray, List[float]]:
+        if self.enable_query_instruction:
+            print("Enabling Query instruction!")
+            queries = [self.query_instruction + " " + query for query in queries]
+        batchified_queries = self._batchify(queries=queries, batch_size=batch_size)
+        query_embeddings = []
+        if show_progress_bar:
+            batches = tqdm(batchified_queries, total=len(batchified_queries))
+        else:
+            batches = tqdm(batchified_queries)
+        for batch in batches:
+            inputs = self._create_ort_input(queries=batch)
+            query_embeddings += list(self.q_model.run([], inputs)[0])
+        query_embeddings = np.asarray(query_embeddings)
+        return query_embeddings
